099d12fc44efe0b9488fca8332ecb555ce249496
chenshaofan <chenshaofan@gbcom.com.cn>
2018-05-14 09:09:32 +0800

    添加习题4.1 tee命令

---
+++ tlpi-dist/fileio/tee.c
@@ -0,0 +1,59 @@
+/*************************************************************************\
+*                  Copyright (C) Michael Kerrisk, 2017.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU General Public License as published by the   *
+* Free Software Foundation, either version 3 or (at your option) any      *
+* later version. This program is distributed without any warranty.  See   *
+* the file COPYING.gpl-v3 for details.                                    *
+\*************************************************************************/
+
+/* Listing 4-1 */
+
+/* copy.c
+
+   Copy the file named argv[1] to a new file named in argv[2].
+*/
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "tlpi_hdr.h"
+
+#ifndef BUF_SIZE        /* Allow "cc -D" to override definition */
+#define BUF_SIZE 1024
+#endif
+
+int
+main(int argc, char *argv[])
+{
+    int inputFd, outputFd, openFlags;
+    mode_t filePerms;
+    ssize_t numRead;
+    char buf[BUF_SIZE];
+
+    if (argc != 2 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s new-file\n", argv[0]);
+
+    /* Open input and output files */
+
+
+
+    openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+    filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+                S_IROTH | S_IWOTH;      /* rw-rw-rw- */
+    outputFd = open(argv[1], openFlags, filePerms);
+    if (outputFd == -1)
+        errExit("opening file %s", argv[1]);
+
+    /* Transfer data until we encounter end of input or an error */
+
+    while ((numRead = read(STDIN_FILENO, buf, BUF_SIZE)) > 0)
+        if (write(outputFd, buf, numRead) != numRead)
+            fatal("couldn't write whole buffer");
+    if (numRead == -1)
+        errExit("read");
+
+    if (close(outputFd) == -1)
+        errExit("close output");
+
+    exit(EXIT_SUCCESS);
+}
